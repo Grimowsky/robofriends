5d2686686e34eb16c0797d92a8d84e8b0f262065
Rafał Mirkowski <rafalmirkowski@gmail.com>
2018-09-21 11:51:58 +0200

    JSONPlaceholder added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import CardList from './CardList'
 import SearchBox from './SearchBox'
-import {robots} from './robots'
+
 import './App.css'
 
 
@@ -9,9 +9,15 @@ class App extends Component {
     constructor(){
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
+    }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response=> response.json())
+            .then(users => this.setState({robots: users}))
     }
 
 onSearchChange = (event) => {
@@ -22,6 +28,9 @@ onSearchChange = (event) => {
         const filteredRobots = this.state.robots.filter(robots => {
         return robots.name.toLowerCase().includes(this.state.searchfield.toLocaleLowerCase())})
 
+        if (this.state.robots.length === 0){
+            return <h1 className='tc'>Loading</h1>
+        } else {
         return(
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
@@ -30,7 +39,7 @@ onSearchChange = (event) => {
             </div>
         )
     }
-
+}
 
 }
 
